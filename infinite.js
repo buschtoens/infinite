081352da2c3a6432ed9f974ad28066f4bac2a4d0
Jan Buschtöns <buschtoens@gmail.com>
2013-01-10 17:59:06 +0100

    Remove append stuff

--- infinite.js
+++ infinite.js
@@ -3,7 +3,8 @@
  */
 
 var events = require("events")
-  , Emitter = require("emitter");
+  , Emitter = require("emitter")
+  , bind = require("bind");
 
 /**
  * Expose `Infinite`.
@@ -14,31 +15,23 @@ module.exports = Infinite;
 /**
  * Initialize a new lazy loading scrolling list.
  *
- * @param {Object} el The DOMElement new content gets appended to.
- * @param {Function} loadCallback The function returning new content.
+ * @param {Object} el The DOMElement to listen of for scroll events.
+ * @param {Function} loadCallback
+ * @param {Number} margin Number of pixels to pre-trigger load.
  * @api public
  */
 
-function Infinite(el, loadCallback) {
+function Infinite(el, loadCallback, margin) {
   this.el = el;
-  this.loadCallback = loadCallback;
+  if(typeof loadCallback == "function")
+    this.on("load", bind(loadCallback, this.el));
   
-  this.margin = 300;
+  this.margin = typeof margin == "number" ? margin : 300;
   this.iteration = 0;
-  
+
   // listen on scroll event
   this.events = events(el, this);
   this.events.bind("scroll");
-
-  // internal event handling
-  this.on("loaded", this.append);
-
-  // loading check
-  this.loading = false;
-  this.ended = false;
-  this.on("load", function() { this.loading = true; });
-  this.on("loaded", function() { this.loading = false; });
-  this.once("end", function() { this.loading = false; this.ened = true; });
 }
 
 // Inherit from Emitter
@@ -53,47 +46,6 @@ Emitter(Infinite.prototype);
 
 Infinite.prototype.onscroll = function() {
   if(this.el.scrollHeight <= this.el.scrollTop + this.el.clientHeight + this.margin) {
-    this.emit("scroll");
-    if(!this.loading && !this.ended) this.load(this.iteration++);
+    this.emit("load", this.iteration++);
   }
-};
-
-/**
- * Load content.
- *
- * @param {Number} iteration
- * @api public
- */
-
-Infinite.prototype.load = function(iteration) {
-  var self = this;
-  
-  this.emit("load", iteration);
-
-  this.loadCallback.call(this.el, iteration, this, function(elements) {
-    if(elements) {
-      if(elements instanceof Array)
-        self.emit("loaded", elements);
-      else
-        self.emit("loaded", [elements]);
-    } else {
-      self.emit("end");
-    }
-    
-    // re-emit scroll event, if the user passed the mark again
-    self.onscroll();
-  });
-};
-
-/**
- * Append content.
- *
- * @param {Array} elements Array of DOMElements
- * @api public
- */
-
-Infinite.prototype.append = function(elements) {
-  elements.forEach(function(element) {
-    this.el.appendChild(element);
-  });
 };
