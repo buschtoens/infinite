b161f1643ddacb34e20b6038e57c0407f08c221b
Jan Buschtöns <buschtoens@gmail.com>
2013-01-10 17:03:37 +0100

    Use events

--- infinite.js
+++ infinite.js
@@ -2,7 +2,8 @@
  * Module dependencies.
  */
 
-var events = require("events");
+var events = require("events")
+  , Emitter = require("emitter");
 
 /**
  * Expose `Infinite`.
@@ -21,15 +22,27 @@ module.exports = Infinite;
 function Infinite(el, loadCallback) {
   this.el = el;
   this.loadCallback = loadCallback;
-	
-	this.margin = 300;
-	this.iteration = 0;
-	this.loading = false;
-	this.empty = false;
-	
-	this.events = events(el, this);
-	this.events.bind("scroll");
+  
+  this.margin = 300;
+  this.iteration = 0;
+  
+  // listen on scroll event
+  this.events = events(el, this);
+  this.events.bind("scroll");
+
+  // internal event handling
+  this.on("loaded", this.append);
+
+  // loading check
+  this.loading = false;
+  this.ended = false;
+  this.on("load", function() { this.loading = true; });
+  this.on("loaded", function() { this.loading = false; });
+  this.once("end", function() { this.loading = false; this.ened = true; });
 }
+
+// Inherit from Emitter
+Emitter(Infinite.prototype);
 
 /**
  * Handle a scroll event.
@@ -39,38 +52,48 @@ function Infinite(el, loadCallback) {
  */
 
 Infinite.prototype.onscroll = function() {
-	if(!this.loading && !this.empty
-	&& this.el.scrollHeight <= this.el.scrollTop + this.el.clientHeight + this.margin) {
-		this.load();
-	}
+  if(this.el.scrollHeight <= this.el.scrollTop + this.el.clientHeight + this.margin) {
+    this.emit("scroll");
+    if(!this.loading && !this.ended) this.load(this.iteration++);
+  }
 };
 
 /**
- * Load content and append it.
+ * Load content.
  *
+ * @param {Number} iteration
  * @api public
  */
 
-Infinite.prototype.load = function() {
-	var self = this;
-	this.loading = true;
-	
-	this.loadCallback.call(this.el, this.iteration++, function(err, elements) {
-		if(err) {
-			if(err == "empty") self.empty = true;
-		} else {
-			if(elements instanceof Array) {
-				elements.forEach(function(element) {
-					self.el.appendChild(element);
-				});
-			} else {
-				self.el.appendChild(elements);
-			}
-		}
-		
-		self.loading = false;
-		
-		// re-emit scroll event, maybe the user past the mark again
-		self.onscroll();
-	});
+Infinite.prototype.load = function(iteration) {
+  var self = this;
+  
+  this.emit("load", iteration);
+
+  this.loadCallback.call(this.el, iteration, this, function(elements) {
+    if(elements) {
+      if(elements instanceof Array)
+        self.emit("loaded", elements);
+      else
+        self.emit("loaded", [elements]);
+    } else {
+      self.emit("end");
+    }
+    
+    // re-emit scroll event, if the user passed the mark again
+    self.onscroll();
+  });
+};
+
+/**
+ * Append content.
+ *
+ * @param {Array} elements Array of DOMElements
+ * @api public
+ */
+
+Infinite.prototype.append = function(elements) {
+  elements.forEach(function(element) {
+    this.el.appendChild(element);
+  });
 };
